3349966daff78c9fdb9d442dc42d4eb721b76ac9
Jesus Miguel Flores <miguelacio@MacBook-Air-de-Jesus.local>
2017-08-19 17:22:44 -0600

    segundo commit con firebase descargado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBtYhZ32EvIdwMJIFvDVnWJTbL46W4cTVQ',
+            authDomain: 'auth-f0ece.firebaseapp.com',
+            databaseURL: 'https://auth-f0ece.firebaseio.com',
+            projectId: 'auth-f0ece',
+            storageBucket: 'auth-f0ece.appspot.com',
+            messagingSenderId: '1088301824987'
+          });
+    }
     render() {
         return (
             <View>
